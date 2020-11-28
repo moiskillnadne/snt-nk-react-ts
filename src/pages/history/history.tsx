7f18fcdca33361d93c7e0c8e7f1790090ee58a8f
victor ryabkov <victor.ryabkov.business@gmail.com>
2020-11-28 17:53:11 +0300

    feature: history page finished

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import '@/pages/history/style.less'
+
+// Components
+const Story = React.lazy(() => import('@/pages/history/content'))
 
 const History = (): JSX.Element => {
-  return <div>История</div>
+  return (
+    <div className="history">
+      <div>
+        <h2 className="page-title">История нашего садоводства</h2>
+      </div>
+
+      <div className="history-video">
+        <iframe
+          title="youtube-video"
+          width="640"
+          height="360"
+          src="https://www.youtube.com/embed/Q7nP-dkgLAg"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+
+      <Suspense fallback={<div>Загрузка..</div>}>
+        <Story />
+      </Suspense>
+    </div>
+  )
 }
 
 export default History
