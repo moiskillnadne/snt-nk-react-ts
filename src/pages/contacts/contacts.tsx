b81b9760130b1d7a864e4cafb6c390070aafb556
victor ryabkov <victor.ryabkov.business@gmail.com>
2020-11-30 07:22:46 +0300

    feature: contacts page finished

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import '@/pages/contacts/style.less'
 
 const Contacts = (): JSX.Element => {
-  return <div>Контакты</div>
+  return (
+    <div className="contacts">
+      <div>
+        <h2 className="page-title">НАШИ КОНТАКТЫ</h2>
+      </div>
+
+      <div className="contacts_item">
+        <p>ЮРИДИЧЕСКИЙ АДРЕС СНТ «НИЖНЯЯ КОЛОНИЯ»</p>
+        <p>188502, Ленинградская область, Ломоносовский район, территория СНТ Нижняя Колония улица Лесная дом 1</p>
+      </div>
+
+      <div className="contacts_item">
+        <p>ПОЧТОВЫЙ АДРЕС СНТ «НИЖНЯЯ КОЛОНИЯ»</p>
+        <p>188502, Ленинградская обл., Ломоносовский р-н, дер. Горбунки, почта, а/я 12</p>
+      </div>
+
+      <div className="contacts_item">
+        <p>Для вопросов и пожеланий можете писать на НАШУ ЭЛЕКТРОННУЮ ПОЧТУ</p>
+        <a href="mailto:nigkolsnt@yandex.ru">nigkolsnt@yandex.ru</a>
+      </div>
+    </div>
+  )
 }
 
 export default Contacts
